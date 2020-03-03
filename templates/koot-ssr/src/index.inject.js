684cae6d30861e58aaee794059b78c89724f9d00
qiyunjiang <qiyunjiang@cmcm.com>
2020-03-03 18:01:25 +0800

    增加publicPath常量

--- templates/koot-ssr/src/index.inject.js
+++ templates/koot-ssr/src/index.inject.js
@@ -4,5 +4,6 @@
 
 export default {
     performanceInfos: () => `<!-- rendered: ${new Date().toISOString()} -->`,
-    svgIconPack: __SVG_ICON_PACK__
+    svgIconPack: __SVG_ICON_PACK__,
+    publicPath: typeof __PUBLICK_PATH__ === 'undefined' ? '' : __PUBLICK_PATH__
 };
