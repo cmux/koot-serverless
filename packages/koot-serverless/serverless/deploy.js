acd21f264fe64f55bc9586ff85ab49f3adb41b0f
qiyunjiang <qiyunjiang@cmcm.com>
2020-03-12 16:53:14 +0800

    修改template/deploy

--- packages/koot-serverless/serverless/deploy.js
+++ packages/koot-serverless/serverless/deploy.js
@@ -4,6 +4,31 @@
  * 发布脚本
  */
 
-const { deploy } = require('koot-serverless');
+const path = require('path');
 
-deploy();
+const spawn = async cmd => {
+    console.log(`=> ${cmd}`);
+    const chunks = cmd.split(' ');
+    await new Promise(resolve => {
+        const child = require('child_process').spawn(chunks.shift(), chunks, {
+            stdio: 'inherit',
+            shell: true,
+            cwd: __dirname
+        });
+        child.on('close', () => {
+            resolve();
+        });
+    });
+};
+
+const run = async () => {
+    const rootPath = process.cwd();
+    const serverlessPath = path.join(rootPath, 'serverless');
+
+    await spawn(`cd ${serverlessPath} && yarn`);
+
+    const { deploy } = require('koot-serverless');
+    await deploy();
+};
+
+run();
