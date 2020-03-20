eb35a59a080c7f3de66eeae5ee7a53c543fe8430
qiyunjiang <qiyunjiang@cmcm.com>
2020-03-20 18:34:40 +0800

    暂时不支持

--- components/tencent-website/serverless.js
+++ components/tencent-website/serverless.js
@@ -8,66 +8,6 @@ class Website extends Component {
     const website = await this.load('@serverless/tencent-website')
     const websiteOutput = await website(inputs)
 
-    // 控制旧版本数量
-    const tencentConf = this.context.instance.credentials.tencent
-    const dirToUploadPath = inputs.code.src || inputs.code.root
-    const uploadDict = {}
-    if (fs.lstatSync(dirToUploadPath).isDirectory()) {
-      uploadDict.dir = dirToUploadPath
-    } else {
-      uploadDict.file = dirToUploadPath
-    }
-    if (uploadDict.dir) {
-      const cos = new COS({
-        SecretId: tencentConf.SecretId,
-        SecretKey: tencentConf.SecretKey,
-        TmpSecretId: tencentConf.SecretId,
-        TmpSecretKey: tencentConf.SecretKey,
-        XCosSecurityToken: tencentConf.token,
-        ExpiredTime: tencentConf.timestamp,
-        UserAgent: 'ServerlessComponent'
-      })
-      // 控制旧版本数量
-      const keepVersion = (inputs.keepVersion || 2) + 1
-      const dir = fs.readdirSync(dirToUploadPath)[0]
-      const match = dir ? dir.match(/(.+?)[\d\-_.|:]+$/) : null
-      const prefix = match ? match[1] : null
-      if (prefix) {
-        let handler
-        let oldVers = []
-        // 查找旧版本
-        try {
-          handler = util.promisify(cos.getBucket.bind(cos))
-          const res = await handler({
-            Bucket: inputs.bucketName,
-            Region: inputs.region,
-            Prefix: prefix
-          })
-          oldVers = res.Contents
-          console.log(res)
-        } catch (e) {
-          throw e
-        }
-        // 删除旧版本
-        console.log(prefix,oldVers)
-        if (oldVers.length > keepVersion) {
-          handler = util.promisify(cos.deleteObject.bind(cos))
-          const arr = oldVers
-            .sort((fst, snd) => {
-              return new Date(fst.LastModified) - new Date(snd.LastModified)
-            })
-            .slice(0, oldVers.length - keepVersion)
-          for (const oldVer of arr) {
-            await handler({
-              Bucket: inputs.bucketName,
-              Region: inputs.region,
-              Key: oldVer.Key
-            })
-          }
-        }
-      }
-    }
-
     return websiteOutput
   }
 
